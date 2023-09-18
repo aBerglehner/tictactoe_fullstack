3ca0d9215decf2ee28bee156411301211c6e3b3e
aBerglehner <alexanderberglehner2@gmail.com>
2023-09-18 23:58:27 +0200

    feat: added test post api at backend and implement in frontend and frontend table with some boilerplate

--- backend/src/main/java/org/acme/GreetingResource.java
+++ backend/src/main/java/org/acme/GreetingResource.java
@@ -1,16 +1,36 @@
 package org.acme;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @Path("/hello")
 public class GreetingResource {
+    static List<String> list = new ArrayList<>();
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Tic tac toe yuhhhh";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response hello() {
+        System.out.println("printed all GAMES get api: " + list);
+        System.out.println("--------------------------------------------------");
+        return Response.ok(list).build();
     }
+
+    @POST
+//    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("/add")
+    public Response addSomething(String game) {
+        System.out.println("Created Game: " + game);
+        System.out.println("--------------------------------------------------");
+
+        list.add(game);
+        return Response.status(201).entity(game).build();
+    }
+
 }
+
--- frontend/src/Content/Content.tsx
+++ frontend/src/Content/Content.tsx
@@ -1,11 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Container from "@mui/material/Container";
 import Paper from "@mui/material/Paper";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import AddBoxIcon from "@mui/icons-material/AddBox";
+import axios from "axios";
+import ShowGames from "./ShowGames";
 
+const defaultGameName = "Game 2";
 const Content = () => {
+  const [game, setGame] = useState(defaultGameName);
+
+  const handleCreateGame = async () => {
+    console.log("handleCreateGame");
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/hello/add",
+        game
+      );
+      console.log("response: ", response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Container
       sx={{
@@ -14,24 +31,27 @@ const Content = () => {
       }}
     >
       {/* Your content goes here */}
-      <Paper elevation={3} sx={{ p: 3 }}>
+      <Paper elevation={3} sx={{ p: 2, minHeight: "84vh" }}>
         <h1>Welcome to Your App</h1>
         <p>This is the body of your app.</p>
 
         <TextField
+          onChange={(e) => setGame(e.target.value)}
           id="outlined-helperText"
           label="Game name"
-          defaultValue="Game 1"
+          defaultValue={defaultGameName}
           helperText="Define your Game name"
           sx={{ marginRight: "5px" }}
         />
         <Button
+          onClick={handleCreateGame}
           variant="contained"
           endIcon={<AddBoxIcon />}
           sx={{ height: "55px" }}
         >
           Create Game
         </Button>
+        <ShowGames />
       </Paper>
     </Container>
   );
